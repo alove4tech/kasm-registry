99602af0a57e6f7d8c38a11b07cc60ad81beaad7
alove4tech <103289104+alove4tech@users.noreply.github.com>
2026-03-31 12:03:09 +0000

    Fix Kasm registry schema discovery and asset paths

--- site/next.config.js
+++ site/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     name: 'Aaron\'s Kasm Registry',
     description: 'Custom ARM-first Kasm workspace registry for Oracle Ampere and other ARM64 Kasm deployments.',
     icon: 'https://alove4tech.github.io/kasm-registry/1.1/favicon.ico',
-    listUrl: 'https://alove4tech.github.io/kasm-registry/1.1/',
+    listUrl: 'https://alove4tech.github.io/kasm-registry/',
     contactUrl: 'https://github.com/alove4tech/kasm-registry/issues',
   },
   reactStrictMode: true,
